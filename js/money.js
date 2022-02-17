5a4024855353b8967fe05e8a951640aa79f1cd0f
afzalhossenhasib <ahhasib47@gmail.com>
2022-02-17 19:22:55 +0600

    error added

--- js/money.js
+++ js/money.js
@@ -9,7 +9,6 @@ function inputFieldValue (inputExpenseId) {
 // All Text Function 
 function inputFeildText (inputText) {
 	const valueText = document.getElementById (inputText);
-	// const amountText = valueText.innerText;
 	return valueText;
 }
 
@@ -19,16 +18,54 @@ const calculateButton = document.getElementById ('caculate-button').addEventList
 		const foodExpenseValue = inputFieldValue ('food-field');
 		const rentExpenseValue = inputFieldValue ('rent-field');
 		const clotheExpenseValue = inputFieldValue ('clothe-field');
-
-		// Total Expense Update 
-		let expenseTotalText = inputFeildText ('total-expense');
-		expenseTotalText.innerText = foodExpenseValue + rentExpenseValue + clotheExpenseValue;
-		
-		// Total Balance Update 
 		const incomeValue = inputFieldValue ('income-field');
-		const incomeFeildText = document.getElementById ('total-balance');
-		incomeFeildText.innerText = incomeValue - (foodExpenseValue + rentExpenseValue + clotheExpenseValue);
 
+		const incomeErrorMessage = inputFeildText ('income-error');
+		const foodErrorMessage = inputFeildText ('food-error');
+		const rentErrorMessage = inputFeildText ('rent-error');
+		const coltheErrorMessage = inputFeildText ('clothe-error');
+
+
+		if (incomeValue < 0) {
+			incomeErrorMessage.style.display = 'inline-block';
+		}
+
+		else if (foodExpenseValue < 0) {
+			foodErrorMessage.style.display = 'inline-block';
+		}
+
+		else if (rentExpenseValue < 0) {
+			rentErrorMessage.style.display = 'inline-block';
+		}
+
+		else if (clotheExpenseValue < 0) {
+			coltheErrorMessage.style.display = 'inline-block';
+		}
+		
+		else {
+			//  Total Expense Update 
+		 	const expenseTotalText = inputFeildText ('total-expense');
+			 const incomeFeildText = document.getElementById ('total-balance');
+
+			 const expense = parseFloat (expenseTotalText)
+
+				const expenseTotal = foodExpenseValue + rentExpenseValue + clotheExpenseValue;
+				expenseTotalText.innerText = expenseTotal;
+				
+				incomeErrorMessage.style.display = 'none';
+			 	foodErrorMessage.style.display = 'none';
+			 	rentErrorMessage.style.display = 'none';
+			 	coltheErrorMessage.style.display = 'none';
+
+			if (incomeValue > expenseTotal) {
+				incomeFeildText.innerText = incomeValue - expenseTotal;
+			}
+
+			else {
+				expenseTotalText.innerText = 'Your Expense Should Be Less Than Your Income';
+			
+			}
+		}
 	 })
 
 	 	// Savaing Amount And Remaing Balance Update 
